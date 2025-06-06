c501b85b619f15815fa3a90041a11528dbfd9158
senyapapa <takeblec@gmail.com>
2025-06-06 17:54:24 +0300

    Add config for nvim

---
+++ lua/chadrc.lua
@@ -0,0 +1,49 @@
+-- This file needs to have same structure as nvconfig.lua
+-- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
+-- Please read that file to know all available options :(
+---@type ChadrcConfig
+local M = {}
+
+local header = require "core.statusline.header"
+
+M.base46 = {
+  theme = "bearded-arc",
+  transparency = true,
+}
+
+M.ui = {
+  statusline = {
+    theme = "vscode_colored",
+  },
+
+  hl_override = {
+    Comment = { italic = true },
+    ["@comment"] = { italic = true },
+    DiffChange = { bg = "#464414", fg = "none" },
+    DiffAdd = { bg = "#103507", fg = "none" },
+    DiffRemove = { bg = "#461414", fg = "none" },
+  },
+}
+M.nvdash = {
+  load_on_startup = true,
+  header = header,
+  buttons = {
+    { txt = "  Find File", keys = "Spc f f", cmd = "Telescope find_files" },
+    { txt = "󰈚  Recent Files", keys = "Spc f r", cmd = "Telescope oldfiles" },
+    { txt = "󰈭  Find Word", keys = "Spc f w", cmd = "Telescope live_grep" },
+    { txt = "  Find Projects", keys = "Spc f p", cmd = "Telescope projects" },
+    { txt = "  Themes", keys = "Spc f t", cmd = "Telescope themes" },
+    { txt = "  Mappings", keys = "Spc n c", cmd = "NvCheatsheet" },
+    { txt = "─", no_gap = true, rep = true },
+    {
+      txt = function()
+        local stats = require("lazy").stats()
+        local milliseconds = math.floor(stats.startuptime) .. " ms"
+        return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. milliseconds
+      end,
+      no_gap = true,
+    },
+    { txt = "─", no_gap = true, rep = true },
+  },
+}
+return M
---
+++ lua/plugins/init.lua
@@ -0,0 +1,76 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre", -- uncomment for format on save
+    config = function()
+      require "configs.conform"
+    end,
+  },
+  {
+    "stevearc/dressing.nvim",
+    lazy = false,
+    opts = {},
+  },
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettier",
+        "eslint-lsp",
+        "gopls",
+        "js-debug-adapter",
+        "typescript-language-server",
+        "clangd",
+        "clangd-format",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "typescript",
+        "javascript",
+        "go",
+      },
+    },
+  },
+  {
+    "mfussenegger/nvim-lint",
+    event = "VeryLazy",
+    config = function()
+      require "configs.lint"
+    end,
+  },
+  {
+    "windwp/nvim-ts-autotag",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-ts-autotag").setup()
+    end,
+  },
+  {
+    "ggandor/leap.nvim",
+    lazy = false,
+    config = function()
+      require("leap").add_default_mappings(true)
+    end,
+  },
+}
